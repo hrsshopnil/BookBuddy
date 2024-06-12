1f22a98e0de8663ce405fb62cd124af5f5ab48cf
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-06-12 21:58:44 +0600

    Initial Commit

---
+++ BookBuddy/BookBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookBuddyApp.swift
+//  BookBuddy
+//
+//  Created by shopnil hasan on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BookBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
